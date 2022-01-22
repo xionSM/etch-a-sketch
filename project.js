9bfec2059324921d3358ac90016e5a37bd2e9d1b
xionSM <be-li-ath@hotmail.com>
2022-01-22 18:36:30 +0200

    "Make/Delete a grid based on user input"

--- project.js
+++ project.js
@@ -1,22 +1,34 @@
 const container = document.getElementById("container");
 const body = document.querySelector("body");
+const container2 = document.createElement("div");
+container2.classList.add("cont");
+body.insertBefore(container2, container);
 
+// Makes a Grid based on the parameters given
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows)
     container.style.setProperty('--grid-cols', cols)
-    for(i = 0; i < (cols * rows); i++) {
+    for(i = 0; i < cols; i++) {
         let cell = document.createElement("div")
-        cell.innerText = (i + 1);
+        cell.textContent = (i + 1);
         container.appendChild(cell).className = "grid-item";
-
+    }
+    let total = (cols * rows) - cols;
+    
+    for (a = 0; a < total; a++) {
+        let cell = document.createElement("div")
+        cell.innerText = cols += 1;
+        container.appendChild(cell).className = "grid-item";
     }
 }
 makeRows(16,16)
 
+// Changes Background Color of each grid to a random one
 function changeColor(e) {
-    e.target.style.backgroundColor = "#22e6fb"
-
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.target.style.backgroundColor = "#" + randomColor;
 }
+
 function changeColor2(e) {
     e.target.style.backgroundColor = "";
 }
@@ -34,6 +46,7 @@ body.insertBefore(button1, container);
 
 let button1Select = document.querySelector('.btn')
 
+// Removes the Grid
 button1Select.addEventListener("click", function() {
     removeAllChildNodes(container)
 })
@@ -49,7 +62,10 @@ button2.innerText = "Create a Grid";
 
 
 body.insertBefore(button2, container) 
+container2.appendChild(button1);
+container2.appendChild(button2);
 
+// Asks for user input to make a grid based on the input
 button2.addEventListener("click", function() {
     let askRows = Number(window.prompt("How many Rows?", ""))
     if (askRows >= 100) {
@@ -69,4 +85,5 @@ button2.addEventListener("click", function() {
     let grids = document.querySelectorAll(".grid-item");
     grids.forEach(grid => grid.addEventListener("mouseenter", changeColor))
     grids.forEach(grid => grid.addEventListener("mouseout", changeColor2))
+    
 })
